748e6503cf83529ad9bbc6d6b5eed765782432a0
Younes ENHARI <45436028+algoyounes@users.noreply.github.com>
2024-06-25 22:04:56 +0100

    tweak: init Idempotency service provider

---
+++ src/IdempotencyServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AlgoYounes\Idempotency;
+
+use Illuminate\Support\ServiceProvider;
+
+class IdempotencyServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/config/idempotency.php' => config_path('idempotency.php'),
+        ], 'config');
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/idempotency.php', 'idempotency');
+    }
+}
