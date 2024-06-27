c8cd5b7f5e4b6a83e48a57d9aef8786195fa00f4
Younes ENHARI <45436028+algoyounes@users.noreply.github.com>
2024-06-27 18:16:07 +0100

    chore: enhance cache store retrieval logic in service provider
    Key Changes :
    - Add `getCacheStoreName` method to encapsulate cache store name retrieval logic
    - Ensure correct handling of `default` cache store configuration
    - Added type checks and fallbacks to guarantee a string is returned for cache store name

--- src/Providers/IdempotencyServiceProvider.php
+++ src/Providers/IdempotencyServiceProvider.php
@@ -4,7 +4,6 @@ namespace AlgoYounes\Idempotency\Providers;
 
 use AlgoYounes\Idempotency\Config\IdempotencyConfig;
 use AlgoYounes\Idempotency\Managers\Cache\IdempotencyCacheManager;
-use AlgoYounes\Idempotency\Managers\IdempotencyManager;
 use Illuminate\Contracts\Cache\LockProvider;
 use Illuminate\Contracts\Cache\Repository as CacheRepository;
 use Illuminate\Support\ServiceProvider;
@@ -25,22 +24,26 @@ class IdempotencyServiceProvider extends ServiceProvider
         $this->app->singleton(
             IdempotencyCacheManager::class,
             function (array $app): IdempotencyCacheManager {
-                $cacheStore = IdempotencyConfig::get(IdempotencyConfig::CACHE_STORE_KEY, 'default');
-                $cacheRepository = $app['cache']->store($cacheStore);
+                $cacheRepository = $app['cache']->store($this->getCacheStoreName());
                 $lockProvider = $app[LockProvider::class];
 
                 return new IdempotencyCacheManager($cacheRepository, $lockProvider);
             }
         );
 
-        $this->app->bind(CacheRepository::class, function (array $app) {
-            $cacheStore = IdempotencyConfig::get(IdempotencyConfig::CACHE_STORE_KEY, 'default');
+        $this->app->bind(CacheRepository::class, fn (array $app) => $app['cache']->store($this->getCacheStoreName()));
 
-            return $app['cache']->store($cacheStore);
-        });
+        $this->app->bind(CacheRepository::class, fn (array $app) => $app['cache']->store($this->getCacheStoreName()));
+    }
 
-        $this->app->singleton(IdempotencyManager::class,
-            fn ($app): IdempotencyManager => new IdempotencyManager($app->make(IdempotencyCacheManager::class))
-        );
+    private function getCacheStoreName(): string
+    {
+        $cacheStore = IdempotencyConfig::get(IdempotencyConfig::CACHE_STORE_KEY, 'default');
+        if ($cacheStore === 'default') {
+            // @phpstan-ignore-next-line
+            return config('cache.default');
+        }
+
+        return $cacheStore;
     }
 }
